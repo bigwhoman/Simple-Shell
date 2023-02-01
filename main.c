e19048d984cd789a1c35904857245a0bef398505
bigwhoman <hm.keshvari@gmail.com>
2023-02-01 11:59:46 +0330

    added a parser and fixed allocation problems

--- main.c
+++ main.c
@@ -7,39 +7,54 @@
 #include <sys/wait.h>
 #include "exec.h"
 #include "colors.h"
+#include <pwd.h>
 
-void execute_command(char *command) {
+char *getcwd(char *buf, size_t size);
+
+void parse_input(char *command) {
+    struct command all_commands[1024];
     char *argv[1024];
     int argc = 0;
+    char *saveptr1, *saveptr2;
+    char *concurrent_command = strtok_r(command, ";", &saveptr1);
+    while (concurrent_command) {
+        argv[argc++] = concurrent_command;
 
-    char *token = strtok(command, " ");
-    while (token) {
-        argv[argc++] = token;
-        token = strtok(NULL, " ");
+        char *part[2000];
+        int part_num = 0;
+        char copy[1024];
+
+        strcpy(copy, concurrent_command);
+        all_commands[argc].parts = (char **) calloc(1024, sizeof(char *));
+        char *command_part = strtok_r(copy, " ", &saveptr2);
+        while (command_part) {
+            all_commands[argc].parts[part_num] = command_part;
+            part[part_num++] = command_part;
+            command_part = strtok_r(NULL, " ", &saveptr2);
+        }
+        all_commands[argc].part_count = part_num;
+        part[part_num] = NULL;
+        concurrent_command = strtok_r(NULL, ";", &saveptr1);
     }
     argv[argc] = NULL;
-
-    pid_t pid = fork();
-    if (pid == 0) {
-        // child process
-        if (execvp(argv[0], argv) < 0) {
-            fprintf(stderr, ANSI_COLOR_RED "Error: Failed to execute command\n" ANSI_COLOR_RESET);
-        }
-        exit(0);
-    } else if (pid > 0) {
-        // parent process
-        int status;
-        wait(&status);
-    } else {
-        fprintf(stderr, ANSI_COLOR_RED "Error: Failed to fork process\n" ANSI_COLOR_RESET);
-    }
+    execute_commands(argc, all_commands);
 }
+
 
 int main(void) {
     char *input;
-
+    char cwd[1024];
+    char *username;
+    uid_t uid = geteuid();
+    struct passwd *pw = getpwuid(uid);
+    char hostname[256];
+    int ret = gethostname(hostname, sizeof(hostname));
     while (1) {
-        input = readline(ANSI_COLOR_GREEN "$ " ANSI_COLOR_RESET);
+        char prompt[2048];
+        char *cwd = getcwd(NULL, 0);
+        sprintf(prompt, "%s%s%s@%s %s~%s%s$ %s", ANSI_COLOR_BLUE, pw->pw_name, ANSI_COLOR_RESET, hostname,
+                ANSI_COLOR_GREEN, cwd, ANSI_COLOR_RESET, ANSI_COLOR_CYAN);
+        char *input = readline(prompt);
 
         if (!input) {
             break;
@@ -49,10 +64,10 @@ int main(void) {
             printf(ANSI_COLOR_YELLOW "Exiting Shell...\n" ANSI_COLOR_RESET);
             exit(0);
         }
-
-        execute_command(input);
+        parse_input(input);
 
         free(input);
+        free(cwd);
     }
 
     return 0;
